e9d660a9ef744cb6546de78135822509790ea914
msvstj <savastijonok@gmail.com>
2024-11-01 20:14:08 +0200

    fix: rpc assignment

--- bin/app.ts
+++ bin/app.ts
@@ -368,7 +368,7 @@ const jsonRpcProviders = {
   WEB3_RPC_8453: process.env.WEB3_RPC_8453!,
   WEB3_RPC_324: process.env.WEB3_RPC_324!,
   WEB3_RPC_11124: process.env.WEB3_RPC_11124!,
-  WEB3_RPC_543210: process.env.WEB3_RPC_11124!,
+  WEB3_RPC_543210: process.env.WEB3_RPC_543210!,
   // The followings are for RPC Gateway
   // Optimism
   // INFURA_10: process.env.INFURA_10!,
